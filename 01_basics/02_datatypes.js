aa896fd6d683d2ea12afca16d79ed2d0a3dd9528
AbHishek9334 <126397781+AbHishek9334@users.noreply.github.com>
2025-03-17 08:05:01 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+"use strict" //treat all js code as newer version
+// alert(3+3) we are using nodejs ,not browser
+console.log(typeof 2)//number
+console.log(typeof "hello")//string
+console.log(typeof true)//boolean
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
