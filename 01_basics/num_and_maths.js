790864f253ebd308e2c53f90a8a4fa2a8c20c83c
AbHishek9334 <126397781+AbHishek9334@users.noreply.github.com>
2025-03-18 15:39:34 +0000

    commit done

--- 01_basics/num_and_maths.js
+++ 01_basics/num_and_maths.js
@@ -19,6 +19,6 @@
 // const min=10
 // const max=20
 // console.log(Math.random()*(max-min+1)+min)
-const min=10
-const max=20
-console.log(Math.floor(Math.random()*(max-min+1)+min))
+// const min=10
+// const max=20
+// console.log(Math.floor(Math.random()*(max-min+1)+min))
--- 02_basics/Arrays.js
+++ 02_basics/Arrays.js
@@ -1,4 +1,4 @@
-const myArr=[10,20,30,40,50]
+// const myArr=[10,20,30,40,50]
 // const myHeroes=["shaktiman","nagraj"]
 // const myArr2=new Array(1,2,3,4)
 // console.log(myArr[1])
@@ -16,10 +16,10 @@ const myArr=[10,20,30,40,50]
 // const newString=myArr.join("-")
 // console.log(typeof newString)
 //slice and splice
-console.log("A",myArr)
-const myn1=myArr.slice(1,3)//slice doesnot modify the original array and last index is excluded`
-console.log(myn1)
-console.log("B",myArr)
-const myn2=myArr.splice(1,3)//splice manipulates the original array and also the last index is included
-console.log(myn2)
-console.log("C",myArr)
+// console.log("A",myArr)
+// const myn1=myArr.slice(1,3)//slice doesnot modify the original array and last index is excluded`
+// console.log(myn1)
+// console.log("B",myArr)
+// const myn2=myArr.splice(1,3)//splice manipulates the original array and also the last index is included
+// console.log(myn2)
+// console.log("C",myArr)
