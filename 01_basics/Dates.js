39c2c2ff7a7652e73b0260d35ec3c787ab872784
AbHishek9334 <126397781+AbHishek9334@users.noreply.github.com>
2025-03-18 03:50:17 +0000

    Date discussed

---
+++ 01_basics/Dates.js
@@ -0,0 +1,33 @@
+// let myDate=new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toISOString())
+// let myCreatedDate=new Date(2025,2,18)
+// console.log(myCreatedDate.toDateString())
+// let myCreatedDate=new Date(2025,2,18,5,3)
+// console.log(myCreatedDate.toLocaleString
+// let myCreateDate=new Date("2025-03-18")
+// console.log(myCreateDate.toLocaleString())
+// let myTimeStamp=Date.now()
+// console.log(myTimeStamp)
+// console.log(myCreateDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+// let newDate=new Date()
+// console.log(newDate.getMonth())
+// console.log(newDate.getDate())
+// console.log(newDate.getDay())
+// console.log(newDate.getFullYear())
+// console.log(newDate.getHours())
+// console.log(newDate.)
+// console.log(newDate.toLocaleString())
+// console.log(newDate.toLocaleString("default",{
+//     weekday:"long",
+//     year:"numeric",
+//     era:"long",
+//     dateStyle:"full"
+    
+// }))
+
