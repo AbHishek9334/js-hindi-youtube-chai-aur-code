97faf0e52e8d65e3046271de343dfd30c0956a0f
AbHishek9334 <126397781+AbHishek9334@users.noreply.github.com>
2025-03-19 23:26:13 +0000

    this and arrow-function discussed

---
+++ 02_basics/this_and_arrowfunction.js
@@ -0,0 +1,31 @@
+// const user={
+//     username:"hitesh",
+//     price:999,
+//     welcomeMessage:function(){
+//         console.log(`${this.username},welcome to website`)
+//         console.log(this)
+//     }
+// }
+// user.welcomeMessage()
+// user.username="sam"
+// user.welcomeMessage()
+// console.log(this)
+// function chai(){
+    
+//     console.log(this)
+// }
+// chai()
+// const chai=()=>{
+//     let username="hitesh"
+//     console.log(this)
+// }
+// chai()
+// const addTwo=(num1,num2)=>{
+//     return num1+num2
+// }
+// result=addTwo(3,4)
+// console.log(result)
+// const addTwo=(num1,num2)=>(num1+num2)
+// console.log(addTwo(3,4))
+// const user=()=>({username:"hitesh"})
+// console.log(user())
