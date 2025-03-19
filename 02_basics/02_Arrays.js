10a0d0234d5bd0c3591af55e1bbc59a2d9108fd5
AbHishek9334 <126397781+AbHishek9334@users.noreply.github.com>
2025-03-19 03:50:10 +0000

    Array-part2 discussed

---
+++ 02_basics/02_Arrays.js
@@ -0,0 +1,20 @@
+const marvel_heroes=["thor","Ironman","spiderman"]
+const dc_heroes=["superman","flash","batman"]
+const numArray=[10,20,30,40]
+// marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes)
+// console.log(marvel_heroes[3][0])
+// newArray=marvel_heroes.concat(dc_heroes)
+// console.log(newArray)
+//spreadout operator
+// const all_new_heroes=[...marvel_heroes,...dc_heroes,...numArray]
+// console.log(all_new_heroes)
+// const anotherArray=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array=anotherArray.flat(Infinity)
+// console.log(real_another_array)
+// console.log(Array.isArray("Hitesh"))
+// console.log(Array.from("Hitesh"))
+score1=100
+score2=200
+score3=300
+console.log(Array.of(score1,score2,score3))
