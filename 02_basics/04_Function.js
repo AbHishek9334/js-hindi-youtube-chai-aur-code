181f045cd407f354fd9afac68ccb799b4f8190b9
AbHishek9334 <126397781+AbHishek9334@users.noreply.github.com>
2025-03-19 22:14:55 +0000

    Functions discussed

---
+++ 02_basics/04_Function.js
@@ -0,0 +1,16 @@
+// function calculateCarPrice(var1,var2,...num1){
+//     return num1
+// }
+// console.log(calculateCarPrice(200,300,400,500,2000))
+// function handleObject(anyObject){
+//     console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+// }
+// handleObject({
+//     username:"sam",
+//     price:399
+// })
+// const myNewArray=[200,400,100,600]
+// function returnSecondValue(getArray){
+//     return getArray[1]
+// }
+// console.log(returnSecondValue(myNewArray))
