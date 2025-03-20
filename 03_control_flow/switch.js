69b6f829111851d3ac954bc65a55166f8ebc31a5
AbHishek9334 <126397781+AbHishek9334@users.noreply.github.com>
2025-03-20 07:43:34 +0000

    Switch case discusseed

---
+++ 03_control_flow/switch.js
@@ -0,0 +1,37 @@
+// const month=7
+// switch (month){
+//     case 1:
+//         console.log("January")
+//         break
+     
+//     case 2:
+//         console.log("February")
+//         break
+//     case 3:
+//         console.log("March")
+//         break
+//     case 4:
+//         console.log("April")
+//         break
+//     default:
+//         console.log("Default case match")
+//         break
+// }
+// const userEmail=[10,20]
+// if(userEmail.length===0){
+//     console.log("Array is empty")
+// }
+// else{
+//     console.log("array is not empty")
+// }
+// const emptyObj={}
+// if(Object.keys(emptyObj).length===0){
+//     console.log("Object is empty")
+// }
+// let val1;
+// val1=5??10
+// val1=null ?? 10
+// val2=undefined ?? 15
+// val3=null ?? 10 ?? 20
+// val4= 0 ?? null ?? true
+// console.log(val4)
