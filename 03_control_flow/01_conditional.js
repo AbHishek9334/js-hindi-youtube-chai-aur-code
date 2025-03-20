49bc25ddeca3d448d44185941a06942c75d16b84
AbHishek9334 <126397781+AbHishek9334@users.noreply.github.com>
2025-03-20 06:00:15 +0000

    if-else discussed

---
+++ 03_control_flow/01_conditional.js
@@ -0,0 +1,30 @@
+// const score=200
+// if(score>100){
+//     let power="fly"
+//     console.log(`User power:${power}`)
+// }
+// console.log(`User power :${power}`)
+const balance=1000
+// if(balance>500) console.log("test")
+// if(balance<500){
+//     console.log("Less than 500")
+// }
+// else if(balance<750){
+//     console.log("less than 750")
+// }
+// else if(balance<900){
+//     console.log("less than 900")
+// }
+// else{
+//     console.log("less than 1200")
+// }
+// const userLoggedIn=true
+// const debitCard=true
+// const loggedInFromGoogle=true
+// const loggedInFromEmail=false
+// if(userLoggedIn && debitCard){
+//     console.log("Allow to buy course")
+// }
+// if(loggedInFromEmail || loggedInFromGoogle){
+//     console.log("User logged in")
+// }
